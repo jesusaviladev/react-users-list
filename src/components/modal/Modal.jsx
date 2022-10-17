6037e9a142d01c9a731c0bf0d0157307453a62bf
jesusaviladev <avijesus13@gmail.com>
2022-10-16 21:39:53 -0400

    modal scroll disabled

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import CrossIcon from '../icons/CrossIcon.jsx';
 import IconButton from '../buttons/IconButton.jsx';
 import style from './Modal.module.css';
 
 const Modal = ({ closeModal, children }) => {
+	useEffect(() => {
+		if (!children) return;
+
+		document.body.classList.add(style.bodyOverflow);
+
+		return () => {
+			document.body.classList.remove(style.bodyOverflow);
+		};
+	}, [children]);
+
 	if (!children) return null;
 
 	return createPortal(
